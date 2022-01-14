400114d7b820049c3aa1314d480810d337276999
Dmitriy Nekludov <nekludov.d.a@gmail.com>
2022-01-14 13:13:57 +0300

    Настраивает сборку

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    open: false,
+    port: 1337,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: "defaults",
+                  },
+                ],
+                "@babel/preset-react",
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
