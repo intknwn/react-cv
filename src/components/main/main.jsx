ac581b33328f2ffa57c7af28c222b8dac547b367
Dmitriy Nekludov <nekludov.d.a@gmail.com>
2022-07-13 22:34:35 +0300

    Добавляет компонент

---
+++ src/components/main/main.jsx
@@ -0,0 +1,17 @@
+import Welcome from "../welcome/welcome.jsx";
+import Skills from "../skills/skills.jsx";
+import Projects from "../projects/projects.jsx";
+import ContactMe from "../contact-me/contact-me.jsx";
+
+const Main = () => {
+  return (
+    <main>
+      <Welcome />
+      <Skills />
+      <Projects />
+      <ContactMe />
+    </main>
+  );
+};
+
+export default Main;
